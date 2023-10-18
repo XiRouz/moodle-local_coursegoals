7d0c832559f18a6cb6d6143fcdbdd128c3334085
semen.lavren <semen.lavren@partner.samsung.com>
2023-10-18 09:24:19 +0300

    WIP 3, added install and uninstall for subplugin

--- comprules/cmcompleted/db/uninstall.php
+++ comprules/cmcompleted/db/uninstall.php
@@ -2,12 +2,12 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-//function xmldb_comprules_cmcompleted_uninstall()
-//{
-//    global $DB;
-//    $factor_name = 'acceptancerate';
-//
-//    $DB->delete_records('sic_kpi_factor', ['name' => $factor_name]);
-//
-//    return true;
-//}
+function xmldb_comprules_cmcompleted_uninstall()
+{
+    global $DB;
+    $comprule_name = 'cmcompleted';
+
+    $DB->delete_records('coursegoals_comprule', ['name' => $comprule_name]);
+
+    return true;
+}
